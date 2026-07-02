840f7d4286d2356584d05fa9d2259a5d7f0a216c
Jee <jee.vim@proton.me>
2026-07-02 07:42:42 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+      <div className='flex'>
+<button className="outline">
+pass
+      </button>
+      <button className="primary">
+      next card
+      </button>  
+      </div>
+      
+      </section>
+    </>
+  )
+}
+
+export default App
