951b13f14111391c3dc9956892c78053a0d6e5e2
Jee <jee.vim@proton.me>
2026-07-02 10:29:55 +0700

    style: update ui card

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,29 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <section>
-      <div className='flex'>
-      <button className="outline">
-      pass
-      </button>
-      <button className="default">
-      next card
-      </button>  
+    <section>
+      <div className='flex-col-between'>
+        <div className='progress'>
+          <p>
+            22 LEFT
+          </p>
+          <div className='percentage'>
+<div className='percentage-bar' style={{width: `70%`}}></div>
+          </div>
+        </div>
+        <div className='card'>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+        </div>
+        <div className='flex'>
+          <button className="outline">
+            pass
+          </button>
+          <button className="primary">
+            next card
+          </button>
+        </div>
       </div>
-      
-      </section>
-    </>
+    </section>
   )
 }
 
